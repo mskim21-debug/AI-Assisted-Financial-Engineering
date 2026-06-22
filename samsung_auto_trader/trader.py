6107a7fc928c97b4522a3be901984d541078d77d
mskim21-debug <265437424+mskim21-debug@users.noreply.github.com>
2026-06-22 22:00:56 +0900

    Update trader.py

--- samsung_auto_trader/trader.py
+++ samsung_auto_trader/trader.py
@@ -1,1 +1,31 @@
+import time
+from market_data import get_price
+from account import get_holdings
+from orders import buy, sell
+from config import BUY_OFFSET, SELL_OFFSET
+from logger import log
 
+
+def run_cycle():
+
+    price = get_price()
+    before = get_holdings()
+
+    buy_price = price - BUY_OFFSET
+    sell_price = price + SELL_OFFSET
+
+    buy(1, buy_price)
+
+    if before["qty"] > 0:
+        sell(before["qty"], sell_price)
+
+    time.sleep(5)
+
+    after = get_holdings()
+
+    if after["qty"] > before["qty"]:
+        log("BUY EXECUTED")
+    elif after["qty"] < before["qty"]:
+        log("SELL EXECUTED")
+    else:
+        log("NO CHANGE")
