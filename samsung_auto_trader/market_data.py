42e2bc2f3c4d1341f8a84e732bb75c71da2173df
mskim21-debug <265437424+mskim21-debug@users.noreply.github.com>
2026-06-22 21:59:11 +0900

    Update market_data.py

--- samsung_auto_trader/market_data.py
+++ samsung_auto_trader/market_data.py
@@ -1,1 +1,22 @@
+from api_client import APIClient
+from config import BASE_URL, SYMBOL
+from logger import log
 
+client = APIClient()
+
+
+def get_price():
+    url = f"{BASE_URL}/uapi/domestic-stock/v1/quotations/inquire-price"
+
+    params = {
+        "fid_cond_mrkt_div_code": "J",
+        "fid_input_iscd": SYMBOL,
+    }
+
+    r = client.get(url, params, tr_id="FHKST01010100")
+    r.raise_for_status()
+
+    price = int(r.json()["output"]["stck_prpr"])
+
+    log(f"Price: {price}")
+    return price
